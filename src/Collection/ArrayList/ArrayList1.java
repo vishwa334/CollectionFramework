aa6d6b718aec10ccb24a0a088c9c1db33bf7c1df
vishwa334 <vk129579@gmail.com>
2024-11-18 19:48:12 +0530

    collection

---
+++ src/Collection/ArrayList/ArrayList1.java
@@ -0,0 +1,18 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        System.out.println("list is : "+ list);
+        System.out.println("Size is :"+list.size());
+    }
+}
---
+++ src/Collection/ArrayList/MergeList.java
@@ -0,0 +1,4 @@
+package Collection.ArrayList;
+
+public class MergeList {
+}
